796287fd3449005af0ca37015d0d77d777ec9526
Bill Li <billli@gmail.com>
2019-07-10 12:51:53 -0400

    im back!

--- main.java
+++ main.java
@@ -1,5 +1,9 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("hi");
+        int sum = 0;
+        for (int i=5; i<=85; i+=4) {
+            sum += i;
+            System.out.println(sum);
+        }
     }
 }
